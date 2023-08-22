b798621511c47aa4e8875f81d169c2b0f4b4d5b4
Avik2903 <76903360+Avik2903@users.noreply.github.com>
2023-08-22 20:16:47 +0530

    question 1

--- question1/src/App.js
+++ question1/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UrlInput from './components/UrlInput';
 
 function App() {
+  const [urls, setUrls] = useState(['']); 
+
+  const handleAddUrl = () => {
+    setUrls([...urls, '']);
+  };
+
+  const handleRemoveUrl = (index) => {
+    const newUrls = [...urls];
+    newUrls.splice(index, 1);
+    setUrls(newUrls);
+  };
+
+  const handleUrlChange = (value, index) => {
+    const newUrls = [...urls];
+    newUrls[index] = value;
+    setUrls(newUrls);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {urls.map((url, index) => (
+        <UrlInput
+          key={index}
+          value={url}
+          onChange={(e) => handleUrlChange(e.target.value, index)}
+          onRemove={() => handleRemoveUrl(index)}
+        />
+      ))}
+      <button onClick={handleAddUrl}>Add URL</button>
     </div>
   );
 }
---
+++ question1/src/components/UrlInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UrlInput = ({ value, onChange, onRemove }) => {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter URL"
+        value={value}
+        onChange={onChange}
+      />
+      <button onClick={onRemove}>Remove</button>
+    </div>
+  );
+};
+
+export default UrlInput;
